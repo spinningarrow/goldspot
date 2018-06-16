3b032b04b1727223e7e001a1101be30ceca62cc6
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-16 18:19:22 +0800

    Fix styling when album art doesn't load

--- src/SongItem.js
+++ src/SongItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default ({ albumArt, title, url }) =>
 	<li className="song-item">
-		<img src={albumArt} alt={title} />
+		<img src={albumArt} alt="album art" />
 		<a href={url} target="_blank">{title}</a>
 	</li>
 
