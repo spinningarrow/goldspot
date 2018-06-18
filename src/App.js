29bbc411612c9142b74755d534755469acf5a59b
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-18 10:28:44 +0800

    Lift data fetching to before rendering App

--- src/App.js
+++ src/App.js
@@ -3,26 +3,6 @@ import React, { Component } from 'react'
 import Header from './Header'
 import SongList from './SongList'
 
-const mapData = data => data.items.map(({
-	played_at: playedAt,
-	added_at: addedAt,
-	track: {
-		id,
-		name: title,
-		album: { images },
-		external_urls: { spotify },
-		artists
-	}
-}) => ({
-	addedAt,
-	albumArt: images[1].url,
-	title,
-	artist: artists[0].name,
-	id,
-	playedAt,
-	url: spotify
-}))
-
 class App extends Component {
 	constructor() {
 		super()
@@ -33,24 +13,12 @@ class App extends Component {
 		}
 	}
 
-	componentDidMount() {
-		fetch('/saved-tracks.json')
-			.then(response => response.json())
-			.then(mapData)
-			.then(recentlyAdded => this.setState({ recentlyAdded }))
-
-		fetch('/recently-played.json')
-			.then(response => response.json())
-			.then(mapData)
-			.then(recentlyPlayed => this.setState({ recentlyPlayed }))
-	}
-
 	render() {
 		return <div className="app">
 			<Header />
 			<main>
-				<SongList heading="Recently Added" items={this.state.recentlyAdded} />
-				<SongList heading="Recently Played" items={this.state.recentlyPlayed.map(({
+				<SongList heading="Recently Added" items={this.props.recentlyAdded} />
+				<SongList heading="Recently Played" items={this.props.recentlyPlayed.map(({
 					albumArt,
 					title,
 					id,
--- src/App.test.js
+++ src/App.test.js
@@ -6,25 +6,7 @@ import App from './App';
 jest.mock('./Header', () => 'Header')
 jest.mock('./SongList', () => 'SongList')
 
-const originalFetch = window.fetch
-
-const mockData = {
-	items: []
-}
-
-beforeEach(() => {
-	window.fetch = () => Promise.resolve({
-		json() {
-			return Promise.resolve(mockData)
-		}
-	})
-})
-
-afterEach(() => {
-	window.fetch = originalFetch
-})
-
 test('it renders without crashing', () => {
-	const tree = renderer.create(<App />).toJSON()
+	const tree = renderer.create(<App recentlyAdded={[]} recentlyPlayed={[]} />).toJSON()
 	expect(tree).toMatchSnapshot()
 })
