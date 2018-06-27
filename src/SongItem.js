665e55d1389de48cf2ea98ab1939b818a2ce9272
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-27 22:59:22 +0800

    Make album art clickable
    Updated styles, let's wait and see if I like them

--- src/SongItem.js
+++ src/SongItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const SongItem = ({ albumArt, title, artist, url }) =>
+const SongItem = ({ albumArt, title, artist, url }) => (
 	<li className="song-item">
-		<img src={albumArt} alt="album art" />
-		<a href={url} target="_blank">{title}</a>
+		<a href={url} target="_blank">
+			<img src={albumArt} alt="album art" />
+			<span className="title">{title}</span>
+		</a>
 		<span className="artist">{artist}</span>
 	</li>
+)
 
 export default SongItem
