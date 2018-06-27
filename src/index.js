3426ba226252063581d4ac719cf8abc495600cdc
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-27 17:08:05 +0800

    Remove unnecessary data fetching from index.js

--- src/index.js
+++ src/index.js
@@ -4,39 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const mapData = data => data.items.map(({
-	played_at: playedAt,
-	added_at: addedAt,
-	track: {
-		id,
-		name: title,
-		album: { images },
-		external_urls: { spotify },
-		artists
-	}
-}) => ({
-	addedAt,
-	albumArt: images[1].url,
-	title,
-	artist: artists[0].name,
-	id,
-	playedAt,
-	url: spotify
-}))
-
-const recentlyAddedPromise = fetch('/saved-tracks.json')
-	.then(response => response.json())
-	.then(mapData)
-
-const recentlyPlayedPromise = fetch('/recently-played.json')
-	.then(response => response.json())
-	.then(mapData)
-
-Promise.all([recentlyAddedPromise, recentlyPlayedPromise]).then(([recentlyAdded, recentlyPlayed]) => {
-	ReactDOM.render(
-		<App recentlyAdded={recentlyAdded} recentlyPlayed={recentlyPlayed} />,
-		document.getElementById('root')
-	);
-})
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+)
 
 registerServiceWorker();
