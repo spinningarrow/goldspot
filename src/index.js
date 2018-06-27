e0643dd816e08985bf0481dd2374eb0523f9a362
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-27 21:44:25 +0800

    Add protocol to Spotify redirect URI
    It's not happy when the protocol isn't there.
    
    Also enabled prettier on save.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 import { getSpotifyToken } from './utils'
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 
 if (!getSpotifyToken()) {
-	const redirectUri = window.location.host
+	const redirectUri = window.location.protocol + '//' + window.location.host
 	const scopes = [
 		'user-library-read',
 		'playlist-read-collaborative',
@@ -19,11 +19,7 @@ if (!getSpotifyToken()) {
 	const spotifyTokenRequestURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=token&state=123`
 	window.location.href = spotifyTokenRequestURL
 } else {
-	ReactDOM.render(
-		<App />,
-		document.getElementById('root')
-	)
+	ReactDOM.render(<App />, document.getElementById('root'))
 }
 
-
-registerServiceWorker();
+registerServiceWorker()
