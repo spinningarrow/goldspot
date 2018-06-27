093fec7f3a106885ee41fe8dc725e6772c73bc8a
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-27 18:44:44 +0800

    Add section to show songs grouped by audio features

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react'
 
 import Header from './Header'
 import SongList from './SongList'
+import MultiSongList from './MultiSongList'
 import withData from './withData'
-import { mapData, mapRecentlyPlayedData } from './mappers'
+import { mapData, mapRecentlyPlayedData, transformAudioFeatures } from './mappers'
 
 const RecentlyAddedSongList = withData(SongList, mapData, '/saved-tracks.json')
 const RecentlyPlayedSongList = withData(SongList, mapRecentlyPlayedData, '/recently-played.json')
+const AudioFeaturesList = withData(MultiSongList, transformAudioFeatures, '/saved-tracks-audio-features.json', '/saved-tracks.json')
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
+			audioFeaturesSongList: 'energize',
 		}
+	}
+
+	handleSelection = (key) => {
+		this.setState({
+			audioFeaturesSongList: key
+		})
 	}
 
 	render() {
@@ -21,9 +30,10 @@ class App extends Component {
 			<main>
 				<RecentlyAddedSongList heading="Recently Added" />
 				<RecentlyPlayedSongList heading="Recently Played" />
+				<AudioFeaturesList selectedSongList={this.state.audioFeaturesSongList} handleSelection={this.handleSelection} />
 			</main>
 		</div>
 	}
 }
 
-export default App;
+export default App
--- src/mappers.js
+++ src/mappers.js
@@ -1,4 +1,4 @@
-import { compose } from 'ramda'
+import { compose, groupBy } from 'ramda'
 
 export const mapData = data => data.items.map(({
 	played_at: playedAt,
@@ -36,3 +36,19 @@ const mapRecentlyPlayed = items => items.map(({
 }))
 
 export const mapRecentlyPlayedData = compose(mapRecentlyPlayed, mapData)
+
+const groupAudioFeatures = groupBy(({ audioFeatures }) => {
+	if (audioFeatures.energy > 0.7 && audioFeatures.danceability < 0.6 && audioFeatures.speechiness < 0.2) return 'energize'
+	if (audioFeatures.danceability > 0.7) return 'dance'
+	return 'other'
+})
+
+const mergeAudioFeatures = (audioFeaturesData, tracksData) => {
+	tracksData.items.forEach((item, index) => {
+		item.track.audioFeatures = audioFeaturesData.audio_features[index]
+	})
+
+	return tracksData
+}
+
+export const transformAudioFeatures = compose(groupAudioFeatures, mapData, mergeAudioFeatures)
