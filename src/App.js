aacd4fd9145ccc9b55eb982e7fd7d2f7b603e55e
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-27 17:05:01 +0800

    Move mappers into their own module

--- src/App.js
+++ src/App.js
@@ -3,44 +3,10 @@ import React, { Component } from 'react'
 import Header from './Header'
 import SongList from './SongList'
 import withData from './withData'
-
-const mapData = data => data.items.map(({
-	played_at: playedAt,
-	added_at: addedAt,
-	track: {
-		id,
-		name: title,
-		album: { images },
-		external_urls: { spotify },
-		artists,
-		audioFeatures
-	}
-}) => ({
-	addedAt,
-	albumArt: images[1].url,
-	title,
-	artist: artists[0].name,
-	id,
-	playedAt,
-	url: spotify,
-	audioFeatures
-}))
-
-const mapRecentlyPlayed = items => items.map(({
-	albumArt,
-	title,
-	id,
-	url,
-	playedAt
-}) => ({
-	id: id + playedAt,
-	albumArt,
-	title,
-	url,
-}))
+import { mapData, mapRecentlyPlayedData } from './mappers'
 
 const RecentlyAddedSongList = withData(SongList, '/saved-tracks.json', mapData)
-const RecentlyPlayedSongList = withData(SongList, '/recently-played.json', json => mapRecentlyPlayed(mapData(json)))
+const RecentlyPlayedSongList = withData(SongList, '/recently-played.json', mapRecentlyPlayedData)
 
 class App extends Component {
 	constructor() {
---
+++ src/mappers.js
@@ -0,0 +1,38 @@
+import { compose } from 'ramda'
+
+export const mapData = data => data.items.map(({
+	played_at: playedAt,
+	added_at: addedAt,
+	track: {
+		id,
+		name: title,
+		album: { images },
+		external_urls: { spotify },
+		artists,
+		audioFeatures
+	}
+}) => ({
+	addedAt,
+	albumArt: images[1].url,
+	title,
+	artist: artists[0].name,
+	id,
+	playedAt,
+	url: spotify,
+	audioFeatures
+}))
+
+const mapRecentlyPlayed = items => items.map(({
+	albumArt,
+	title,
+	id,
+	url,
+	playedAt
+}) => ({
+	id: id + playedAt,
+	albumArt,
+	title,
+	url,
+}))
+
+export const mapRecentlyPlayedData = compose(mapRecentlyPlayed, mapData)
