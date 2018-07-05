54b353ccb0ae4d11639df408799a541d7f19104e
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-05 11:49:32 +0800

    Add broken test

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,10 @@ import App from './App';
 
 const renderer = new ShallowRenderer()
 
+test('break test to see CI fail', () => {
+	expect(1).toBe(2)
+})
+
 test('it renders without crashing', () => {
 	const tree = renderer.render(<App recentlyAdded={[]} recentlyPlayed={[]} />)
 	expect(tree).toMatchSnapshot()
