846b0cecf6d2ef1de7bf39df148a712fb698dc66
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-26 21:21:47 +0800

    Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from 'react'
 import ShallowRenderer from 'react-test-renderer/shallow'
 
-import App from './App';
+import App from './App'
 
 const renderer = new ShallowRenderer()
-
-test('break test to see CI fail', () => {
-	expect(1).toBe(2)
-})
 
 test('it renders without crashing', () => {
 	const tree = renderer.render(<App recentlyAdded={[]} recentlyPlayed={[]} />)
