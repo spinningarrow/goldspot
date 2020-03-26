afe1f6b582c087b982ec2453fc9656e40f6be4d4
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2020-03-26 14:49:42 +0100

    Remove unused api functions
    Moved to graphql

--- src/api.js
+++ src/api.js
@@ -1,37 +1,5 @@
-import {
-	mapData,
-	mapRecentlyPlayedData,
-	transformAudioFeatures,
-} from './mappers'
+import { transformAudioFeatures } from './mappers'
 import { getSpotifyToken } from './utils'
-
-export const getTracks = async () => {
-	const tracksResponse = await fetch(
-		'https://api.spotify.com/v1/me/tracks?limit=50',
-		{
-			headers: {
-				Authorization: `Bearer ${getSpotifyToken()}`,
-			},
-		}
-	)
-	const tracks = await tracksResponse.json()
-
-	return mapData(tracks)
-}
-
-export const getRecentlyPlayed = async () => {
-	const tracksResponse = await fetch(
-		'https://api.spotify.com/v1/me/player/recently-played?limit=50',
-		{
-			headers: {
-				Authorization: `Bearer ${getSpotifyToken()}`,
-			},
-		}
-	)
-	const tracks = await tracksResponse.json()
-
-	return mapRecentlyPlayedData(tracks)
-}
 
 export const getAudioFeatures = async () => {
 	const tracksResponse = await fetch(
@@ -70,30 +38,4 @@ export const playTrack = id => event => {
 			uris: [`spotify:track:${id}`],
 		}),
 	})
-}
-
-export const getNowPlaying = async () => {
-	try {
-		const response = await fetch(
-			'https://api.spotify.com/v1/me/player/currently-playing',
-			{
-				headers: {
-					Authorization: `Bearer ${getSpotifyToken()}`,
-				},
-			}
-		)
-
-		const {
-			is_playing,
-			item: { artists, name },
-		} = await response.json()
-
-		return {
-			artist: artists && artists[0].name,
-			trackName: name,
-			isPlaying: is_playing,
-		}
-	} catch {
-		return {}
-	}
 }
