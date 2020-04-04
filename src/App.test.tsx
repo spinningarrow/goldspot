933f28d769054cfad2b4f60d64f848746fa54cfc
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2020-04-04 20:21:52 +0200

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { wait, render } from '@testing-library/react'
 
 import App from './App'
 
 it('renders without crashing', () => {
-	const { baseElement } = render(
-		<App />
-	)
+	const { baseElement, findByText } = render(<App />)
+
+	findByText('Gold') // wait for async stuff to finish
 
 	expect(baseElement).toHaveTextContent('Gold')
 	expect(baseElement).toHaveTextContent('Recently Played')
--- src/Header.test.tsx
+++ src/Header.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
 
 import Header from './Header'
 
 it('renders a heading with the app name', () => {
-	const { baseElement } = render(<Header />)
+	const { baseElement } = render(
+		<MockedProvider>
+			<Header />
+		</MockedProvider>
+	)
 
 	expect(baseElement).toHaveTextContent('Goldspot')
 })
